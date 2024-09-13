698b694801a60a0054c89faba8d78a24f0d05d95
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-09-13 18:23:25 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score= "brijesh"
+
+// console.log(typeof score);
+// console.log(type(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+
+// "33"=>33
+//"33abc"=>Nan
+//"true"=>1;false=> 0
+
+let isLoggedIn = "brijesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true ;0=> false 
+//""=>false
+//"brijesh"=> true
+
+
+let someNumber= 33
+
+let StringNumber=String(someNumber)
+console.log(StringNumber);
+
+
+
+console.log(typeof StringNumber);
+
+
