e76559b4ac8a49eb7bfed0a496362e00439a9b02
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-09-13 10:57:47 +0000

    datatype

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId= 12321
 let accountEmail= "awigrawfj232@gmail.com"
 var accountPassword="21232"
 accountCity= "Delhi"
-
+let accountstate;
 
 //account= 2 // not allowed
 
@@ -10,4 +10,16 @@ accountEmail="brijeshsahanai22ocr@gmail.com"
 accountPassword="8762476289"
 accountCity="Gujarpar"
 
-console.log(accountId,accountEmail,accountPassword,accountCity)
+console.log(accountId);
+
+/*
+
+prefer  not  to use var 
+because of issue in block scope and fuctional scope
+
+*/
+
+console.log(accountId,accountEmail,accountPassword,accountCity, accountstate)
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"us strict";  // treat all JS code as newer version
+
+
+// alert (3+3) we are using node JS not browers
+
+
+console.log(3
+    +
+    3) // code readabilty should be high 
+
+let name= "brijesh"//string
+let age= 19.3 // number
+let isLoggedIn = false 
+let state;
+
+
+// number => 2 to 53
+//bigit
+//string=.""
+//boolean=>false/true
+//null =>standlone value
+//undefined=> unique
+
+//object 
+
+console.log (typeof undefined);// undefined
+console.log(typeof null); // object
+
+
+
+
+
+
+
+
