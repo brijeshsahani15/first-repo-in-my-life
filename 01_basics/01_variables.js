7e2f559d8a4c422a59446e32af29912dc3ec92c0
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-09-13 06:16:22 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 12321
+let accountEmail= "awigrawfj232@gmail.com"
+var accountPassword="21232"
+accountCity= "Delhi"
+
+
+//account= 2 // not allowed
+
+accountEmail="brijeshsahanai22ocr@gmail.com"
+accountPassword="8762476289"
+accountCity="Gujarpar"
+
+console.log(accountId,accountEmail,accountPassword,accountCity)
