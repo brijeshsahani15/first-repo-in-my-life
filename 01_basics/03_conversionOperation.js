d0f8a437dd5f48a0e7d31b6937bc4691e98bd328
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-09-14 10:19:04 +0000

    conversionOf data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score= "brijesh"
 
-// console.log(typeof score);
-// console.log(type(score));
+console.log(typeof score);
+console.log(type(score));
 
 let valueInNumber = Number(score)
 
-// console.log(typeof valueInNumber);
+console.log(typeof valueInNumber);
 
 
 // "33"=>33
@@ -30,5 +30,34 @@ console.log(StringNumber);
 
 
 console.log(typeof StringNumber);
+
+
+/////////***************operations**************************// 
+
+let value = 3
+let negvalue =-value
+
+
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2/3);
+console.log(2%3);
+
+ console.log(+true);
+ console.log(+"");
+
+ let num1 , num2, num3
+ num1= num2=num3 = 2+3
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
+
+
 
 
