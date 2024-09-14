ab091783d456efa8df233bfcb85cbbcee88fb991
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-09-14 14:28:07 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive
+
+//types: string ,number,null ,undefined,boolean,synbole,BigInt
+
+
+const score = 100
+
+const scoreValue= 100.2
+const isLoggedIn= false 
+const outsideTemp= nulll 
+let userEmail;
+
+
+const id= symbol('123')
+const anotherId = symbol('123')
+
+console.log(id=== anotherId);
+
+const bigNumber= 486369826823668
+
+
+// refrence (non primitive)
+
+//Arrays,objects,functions
+
+
+const heroes= ["shaktiman", "naagraj","flying"]
+ let myOb={ 
+    name:"brijesh",
+    age: 19.3,
+}
+
+const myFunction =function(){
+    console.log("hello world!");
+
+
+}
+    
+
+console.log(typeof bigNumber);
+
+
