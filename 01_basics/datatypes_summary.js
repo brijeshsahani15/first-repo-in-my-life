aeb9536a85c4a779f886a69efdeadcb349503a62
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-09-15 16:58:09 +0000

    datatypes sumarry

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,62 @@
+// primitive
+
+// types: string ,number,null ,undefined,boolean,synbole,BigInt
+
+
+// const score = 100
+
+// const scoreValue= 100.2
+// const isLoggedIn= false 
+// const outsideTemp= nulll 
+// let userEmail;
+
+
+// const id= symbol('123')
+// const anotherId = symbol('123')
+
+// console.log(id=== anotherId);
+
+// const bigNumber= 486369826823668
+
+
+// // refrence (non primitive)
+
+// // Arrays,objects,functions
+
+
+// const heros= ["shaktiman", "naagraj","flying"];
+//  let myObj={ 
+//     name:"brijesh",
+//     age: 19,
+// }
+
+// const myFunction =function(){
+//     console.log("hello world");
+// }
+    
+
+// console.log(typeof heros);
+
+
+////++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// stack (primitive), heap (non primitive)
+
+// let myYoutubeName="brijeshsahani"
+
+// let anotherName= "chaiaurcode"
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+
+let userOne ={
+    email:"brijeshshani@222oct.gmail.com",
+    upi:"brijeshsahani9011@ubi",
+}
+
+let userTwo= userOne
+userTwo.email="aaravnnishad63@gmai.com"
+
+console.log(userOne.email);
+console.log(userOne.email);
