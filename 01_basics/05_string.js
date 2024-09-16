ccbcae78eaa189e8bbe0bd13dd5bb8c66d372a64
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-09-16 18:19:08 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,7 +1,24 @@
 const name= "krishna"
 const repoCount= 50
-// console.log(name + repoCount + "value");
+console.log(name + repoCount + "value");
 
 
 
-console.log('  hello my name is ${name} and my repo count is $ {repoCount}');
+console.log('hello my name is ${name} and my repo count is ${repoCount}');
+
+
+const gameName= new String('brijesh-neelam')
+console.log(gameName[0]);
+console.log('gameName.__proto__');
+
+
+
+console.log(gameName.length);
+console.log(gameName.toLocaleLowerCase());
+
+console.log(gameName.charAt(0));
+
+console.log( gameName.indexOf('h'));
+
+
+
