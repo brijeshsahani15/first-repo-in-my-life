2cbd9bb40373b6144c73c3abe03bf8c6d2b4a8dd
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-09-21 15:30:30 +0000

    nums_and_maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,12 @@
+const score= 400
+
+console.log(score);// its output become 400
+
+const balance= new Number(100.324)
+console.log(balance);//its output become [number:100]  above number see the clearly difference between them
+
+
+
+
+console.log(balance.toString().length);
+console.log(balance.toString(2));
