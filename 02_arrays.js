a48efbf8459975619841901ba3aff8b16cdac87c
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-10-01 00:22:01 +0530

    Add files via upload

---
+++ 02_arrays.js
@@ -0,0 +1,69 @@
+// const marvel_heros=["thor","spiderman", "captian"]
+
+
+// const dc_heros = ["super","wanda","flash"] 
+
+// // marvel_heros.push(dc_heros);
+
+// // console.log(marvel_heros);
+
+// // console.log(marvel_heros[2][1]);
+
+// // const allheros=marvel_heros.concat(dc_heros);///to merge two arrays  dc heros and marvel heros
+// // console.log(allheros);
+
+
+// const all_new_heros= [...marvel_heros,...dc_heros];///(spread mehtod)easy method to merge tow two array in easy way  and correct way  above method are same 
+
+// console.log(all_new_heros);
+
+
+// const another_array=[1,2,3,[4,5,6,7[6,7,8]]]
+
+// const real_another_array = another_array.flat(3);
+
+// console.log(real_another_array);
+
+
+// console.log(typeof real_another_array);
+
+// console.log(Array.isArray("brijesh"))//to check the array (boolean) true or false 
+// console.log(Array.from("brijesh"))//to convert string into array
+
+// console.log(Array.from({name:"brijesh"}));//to convert object into array its print ( [] ) this is important quetion related to interview
+
+// let score1= 100
+// let score2= 200
+// let score3= 300
+
+
+// console.log(Array.of(score1, score2, score3,));
+
+
+let array = ["Hello", "GeeksforGeeks", "JavaScript"];
+console.log(array.indexof("hello","javascript"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
