5ffaf0393a8c47e35d992864b01473567e685dab
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-10-02 18:21:49 +0000

    01_functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+
+function sayMyName(){
+    console.log("b");
+    console.log("r");
+    console.log("i");
+    console.log("j");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+
+}
+// sayMyName()
+
+
+function addTwoNumbers(number1,number2){
+
+    console.log(number1+number2);
+}
+addTwoNumbers(3,4)
