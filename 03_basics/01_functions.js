9498215551e00ef30ce1cf0e9b744af0fc7f8834
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-10-07 18:40:07 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,19 +1,41 @@
 
-function sayMyName(){
-    console.log("b");
-    console.log("r");
-    console.log("i");
-    console.log("j");
-    console.log("e");
-    console.log("s");
-    console.log("h");
+// // function sayMyName(){
+// //     console.log("b");
+// //     console.log("r");
+// //     console.log("i");
+// //     console.log("j");
+// //     console.log("e");
+// //     console.log("s");
+// //     console.log("h");
+    
+  
 
+// // }
+
+// // sayMyName()
+
+
+// // function addWant(number1,number2){
+// //     console.log(number1 + number2);
+  
+// // }
+
+
+// function addWant(number1,number2){
+//     // let result=number1 + number2
+//     // return result         /// this is first method 
+
+//     return number1+number2   ///this another method to return the number
+// }
+
+//  const result = addWant(22.3,2);
+// //  console.log("result:",result);
+
+
+function userLoginName(userName){
+    return `${userName} user logged in`;
+
+   
 }
-// sayMyName()
+ console.log(userLoginName("brijesh"));
 
-
-function addTwoNumbers(number1,number2){
-
-    console.log(number1+number2);
-}
-addTwoNumbers(3,4)
