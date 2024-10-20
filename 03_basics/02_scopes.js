14ecf9c5b627cb3d5cc0011b04d1431272dbc6f2
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-10-20 05:27:45 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,12 +3,45 @@
 
 let a=3000
 if (true){
-    let a= 200
+    let a= 10
+    const b= 200
+    console.log("INNER:", a);
+
    
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username="brijesh"
+
+    function two(){
+        const website= "github"
+        console.log(username);
+    }
+    // console.log(username)
+
+    two()
+}
+
+one()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
