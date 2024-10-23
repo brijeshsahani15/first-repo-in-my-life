071ab6b2684f7462a618940981ea78c276394a4c
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-10-23 17:33:38 +0000

    controls

--- 04_controls/one.js
+++ 04_controls/one.js
@@ -1,15 +1,79 @@
-///if 
-const IsUserLogedIn= true
-temperature  = 50
+// ///if 
+// const IsUserLogedIn= true
+// temperature  = 50
 
 
-if (temperature = 41){
-    console.log("less than 40");
+// if (temperature = 41){
+//     console.log("less than 40");
 
-} else {
-    console.log("temperature  is greater than 50");
+// } else {
+//     console.log("temperature  is greater than 50");
+// }
+// console.log("executed");
+
+
+
+
+// const score = 200;
+// if (score > 100) {
+//     const power = "fly";   //// if we are using the "var" then print outside of funtion
+//     console.log(`User power: ${power}`); 
+// }
+
+
+// console.log(`User power: ${power}`);
+
+
+
+
+// const balance = 1000
+// if (balance <500) {
+//     console.log("less than 750");
+// } else  if (balance < 850){
+//     console.log("less  than 850 ");
+// } else {
+//     console.log("less than 1300 ");
+// }
+
+////// this above  code try for multiple condition in this way
+
+
+
+
+const IsUserLogedIn = true 
+const debitCard = true 
+const IsUserLogedInEmail= false 
+const loggedInFromGoogle= true
+
+
+
+
+if (IsUserLogedIn && debitCard){
+    console.log("buy a  cruish missile");
 }
-console.log("executed");
+
+if (loggedInFromGoogle||IsUserLogedInEmail ){
+    console.log("Usser logged in");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
