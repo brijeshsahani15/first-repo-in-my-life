18a21eef6c348037e2f4faa64a44cc558b5fb153
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-10-25 14:20:15 +0530

    switch and truthy

---
+++ 04_controls/switch.js
@@ -0,0 +1,178 @@
+
+
+
+
+const month = 3;
+switch (month) {
+    case 1: 
+        console.log("january");
+        
+        break;
+    case 2: 
+        console.log("feb");
+        break;
+    case 3: 
+        console.log("march");
+        break;
+    case 4: 
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
+
+const dayNumber= 3
+switch (dayNumber){
+    case 0:
+        console.log('its is sunday');
+        break;
+
+    case 1:
+        console.log('its is monday');
+        break;
+
+    case 2:
+        console.log('tuesday');
+        break;
+
+    case 3:
+        console.log(wednesday);
+        break;
+
+    case 4:
+        console.log('thursday');
+        break;
+
+    case 5:
+        console.log('friday');
+        break;
+
+    case 6:
+        console.log('saturday');
+        break;
+    
+    default:
+        console.log('please enter the valid number ') ;
+        break;
+
+
+}
+
+console.log('end the program');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
