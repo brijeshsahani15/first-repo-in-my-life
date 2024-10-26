e3bb9d11536245bdba03a71e2955f317a6a0cb49
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-10-26 17:39:50 +0000

    iterations.js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -13,10 +13,35 @@ for (let i = 0; i <=10; i++) {
 for (let i = 0; i <=10 ; i++) {
     console.log(`outer loop value: ${i}` );
     for (let j = 0; j <=10; j++) {
-        console.log(` Inner loop value ${j} and inner loop ${i}`);
+        // console.log(` Inner loop value ${j} and inner loop ${i}`);
+        console.log(i+'*' + j ` = ` + i*j);
        
-        
+    
     }
-    
-    
+     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
