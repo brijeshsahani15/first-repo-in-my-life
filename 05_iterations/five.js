be274ca282af3532063374569871dbdc0e04e56c
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2024-11-05 19:03:46 +0000

    iterations

---
+++ 05_iterations/five.js
@@ -0,0 +1,66 @@
+const coding  = ["js", "python","java","cpp"]
+
+
+
+
+// coding. forEach( function (val){
+//     console.log(val);
+
+// })
+
+
+
+
+
+/// this is the second way to access the  conding value 
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+
+// coding .forEach(printMe);
+
+
+
+
+
+// this is the arrow functions(it can uasee in many way) 
+
+// coding .forEach((item,index, arr)=>{
+//     console.log(item,index ,arr);
+
+
+// })
+
+
+
+
+/// array in object    [{}] 
+
+
+const myCoding =[
+    {
+        languageName:"javascript" ,
+        languageFileName:"js"
+    },
+   
+    {
+        languageName:"java" ,
+        languageFileName:"java"
+    },
+    {
+        languageName:"python" ,
+        languageFileName:"py"
+    }
+
+]
+
+
+myCoding.forEach ((item) => {
+
+    console.log(item.languageName);
+
+})
+
