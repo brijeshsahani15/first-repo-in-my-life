388c2dfa1717b7500263f8daa64a89b96a925a6b
brijeshsahani15 <brijeshsahani222oct@gmail.com>
2025-01-12 12:11:29 +0000

    objects.js

---
+++ 10_classes_and_oop/objects.js
@@ -0,0 +1,10 @@
+function multiplyBy5(num){
+    returnnum*5
+}
+multiplyBy5.power=2
+console.log(multiplyBy5(5))
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype)
+function creatUser(username,score){
+    this .username=username
+}
